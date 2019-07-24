2d7e00831339be59e425d7ee13f2cf6c9dfb9d7c
Carly <carlyc999@gmail.com>
2019-07-24 16:26:02 -0700

    more stuff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webapp2
 import os
 import jinja2
+users = {'firstName': {}, 'lastName': {}, 'email': {}, 'password': {}}
 
 the_jinja_env = jinja2.Environment(
      loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -13,20 +14,29 @@ class MainHandler(webapp2.RequestHandler):
         landing_template = the_jinja_env.get_template("/templates/index.html")
         self.response.write(landing_template.render())
 
+    def post(self, firstName, lastName, email, password):
+        self.firstName = self.request.get('firstName')
+        self.lastName = self.request.get('lastName')
+        self.email = self.request.get('email')
+        self.password = self.request.get('password')
+        for i in range (len(users) + 2):
+            if i > len(users):
+                firstName.update(self.firstName)
+                lastName.update(self.lastName)
+                email.update(self.email)
+                password.update(self.password)
+        self.response.write("yay!")
+
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         login_template = the_jinja_env.get_template("/templates/login.html")
         self.response.write(login_template.render())
-
-# class SignUpHandler(webapp2.RequestHandler):
-#     def get(self):
-#         pass
-#     def post(self):
-#         pass
+    def post(self):
+        self.response.write("Congrats!")
 
 class DashHandler(webapp2.RequestHandler):
     def get(self):
-        dash_template = the_jinja_env.get_template("/templates/dashBoard.html")
+        dash_template = the_jinja_env.get_template("/templates/dashboard.html")
         self.response.write(dash_template.render())
     def post(self):
         self.response.write("Hello")
