46c1f156de92c801b1e25f4f6adab5aa0f04f166
Ethan Mooney <ethanimooney@gmail.com>
2019-07-24 08:56:07 -0700

    Few edits, pushed to appengine, added favicon

--- main.py
+++ main.py
@@ -2,11 +2,35 @@ import webapp2
 import os
 import jinja2
 
-jinja_current_directory = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+the_jinja_env = jinja2.Environment(
+     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+     extensions = ['jinja2.ext.autoescape'],
+     undefined = jinja2.StrictUndefined,
+     autoescape = True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        landing_template = the_jinja_env.get_template("/templates/index.html")
+        self.response.write(landing_template.render())
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        login_template = the_jinja_env.get_template("/templates/login.html")
+        self.response.write(login_template.render())
+
+# class SignUpHandler(webapp2.RequestHandler):
+#     def get(self):
+#         pass
+#     def post(self):
+#         pass
+
+class DashHandler(webapp2.RequestHandler):
+    def get(self):
+        dash_template = the_jinja_env.get_template("/templates/dashBoard.html")
+        self.response.write(dash_template.render())
 
 app = webapp2.WSGIApplication([
-    ('/fortune', FortuneHandler)
+    ('/', MainHandler),
+    ('/login', LoginHandler),
+    ('/dashboard', DashHandler),
 ], debug=True)
